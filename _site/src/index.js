77949a95dd8bc03df5bbd773e9c181ac435cb7f3
Jacqueline H <jacqueline.hsiang@gmail.com>
2026-08-01 19:44:27 -0600

    fonts

---
+++ _site/src/index.js
@@ -0,0 +1,23 @@
+import Contact from './sections/Contact';
+import "normalize.css";
+import "./scss/main.ltr.scss";
+import "./scss/themes.scss";
+import "./scss/purple.scss";
+import "./scss/custom.scss";
+
+import imagesLoaded from "imagesloaded";
+
+import SiteState from './js/SiteState';
+import Site from './js/Site';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // ... any other classes you are initializing ...
+    
+    // Initialize the contact form logic
+    const contactForm = new Contact();
+});
+
+imagesLoaded('.preloadimage', function() {    
+    const siteState = new SiteState();
+    new Site(siteState);
+});
